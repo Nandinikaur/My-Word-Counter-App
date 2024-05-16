bbc3232dfcd07423d5deaa230c342f9477cdaa89
Nandini Kaur <160272450+Nandinikaur@users.noreply.github.com>
2024-05-16 09:34:00 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+//Nandini_Kaur 06-05-2024
+
+import inquirer from "inquirer";
+import chalk from "chalk"; 
+
+
+console.log(chalk.bold.blue(`\n\t>>>>>>***WELCOME TO MY WORD-COUNTER APP***<<<<<</t\n\n`));
+
+ let word_counter = await inquirer.prompt([
+    {
+        name: "words",
+        type: "input",
+        message: chalk.bgBlue("a: Enter some text"),
+    }
+ ]);
+
+ let countWords = word_counter.words.trim().split(" ").length
+
+ console.log(chalk.green.bold(`\n\tYou entered ${chalk.bold.yellow(countWords)} word(s).\t\n`));
